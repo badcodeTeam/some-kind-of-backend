b33895ab99831432b4e27607ccefcb1307180755
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-09-06 18:00:56 +0300

    update

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,20 +1,23 @@
 import { Module } from "@nestjs/common";
-import {AppController} from "./app.controller"
-import {AppService} from "./app.service"
+import { ConfigModule } from "@nestjs/config";
 import {SequelizeModule} from "@nestjs/sequelize"
-import { UsersModule } from './users/users.module';
+import { UsersModule } from "./users/users.module";
+
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
         SequelizeModule.forRoot({
         dialect: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'root',
-        database: 'nest-server',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
         models: [],
         autoLoadModels: true
         }),
