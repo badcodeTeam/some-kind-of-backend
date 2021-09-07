29789bbc920723f9f0102ebc67a501923ce56b5a
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-09-07 15:58:56 +0300

    Добавлена валидация

--- nest-server/src/users/dto/add-role.dto.ts
+++ nest-server/src/users/dto/add-role.dto.ts
@@ -1,11 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
 
 
 export class AddRoleDto {
 
     @ApiProperty({example: 'ADMIN', description: 'Роль'})
+    @IsString({message: 'Должно быть строкой'})
     readonly value: string;
 
     @ApiProperty({example: 'Администратор', description: 'Описание роли'})
+    @IsNumber({}, {message: 'Должно быть числом'})
     readonly userId: number;
 }
